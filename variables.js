d9f04f41543c8da581080e431e61ba27f70a8eed
Alex Aaron <alexfrancisaaron@gmail.com>
2024-09-17 17:56:50 +0000

    added sections and variables descriptions

---
+++ variables.js
@@ -0,0 +1,11 @@
+/**
+ * 0: Variables are named containers for storing data. (... add a few more sentences to describe variables)
+ * 
+ * 1. Declaration and Assignment: In order to create a variable we must first `declare` it. Depending on the declarative keyword you use, you may initialize
+ * the variable with a value or not. 
+ * 
+ * 2. Var, let, const: There are three declarative keywords that can be used to declare variables--var, let, const. (.. describe each keyword in detai)
+ * 
+ * 3: Hoisting: (... describe hoisting in detail)
+ */
+
