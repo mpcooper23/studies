290fc04a3bfe434b85af36834a91f8991cf128f4
mpcooper23 <mpcooper23@gmail.com>
2024-11-09 13:24:32 -0600

    q

--- functions.js
+++ functions.js
@@ -1,10 +1,10 @@
 /**
  * Functions
  * 
- * 0: Functions are pieces of reusable code that can be invoked at any point in a program. Like simple data 
- * declared through variables, a function exists as a piece of data stored in memory. Functions differ in that
- * they can be invoked with arguments. The functions parameters act as placeholders, or slots waiting to be filled with
- * values that are called in the argument. 
+ * 0: Functions are pieces of reusable code (e.g. "set of instructions") that can be invoked at any point in a program. 
+ * Like simple data declared through variables, a function exists as a piece of data stored in memory. Functions differ 
+ * in that they can be invoked with arguments, or '"called" with data placed between the syntax's parentheses. The 
+ * function's parameters act as placeholders, or slots waiting to be filled with the argument values. 
  * 
  * 1. 
  * 
