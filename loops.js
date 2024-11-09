d9aa3fd6e3a8e25b3fa478e0db9aa544741d4530
mpcooper23 <mpcooper23@gmail.com>
2024-11-09 13:42:12 -0600

    a

--- loops.js
+++ loops.js
@@ -3,9 +3,11 @@
  * 
  * 0: 
  * 
- * 1. 
+ * 1. Explain while, for, and for-in loops.
  * 
- * 2. 
+ * 2. Be able to loop any number of times, forward counting up to some limit, backward counting down to 0.
  * 
- * 3: 
+ * 3. Loop over an Array, forwards and backwards.
+ * 
+ * 4. Loop over an Object.
  */
