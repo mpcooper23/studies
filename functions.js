a3d258461acb1eb5f06a171084c0d79f643c0140
mpcooper23 <mpcooper23@gmail.com>
2024-11-09 13:51:28 -0600

    w

--- functions.js
+++ functions.js
@@ -6,9 +6,17 @@
  * in that they can be invoked with arguments, or '"called" with data placed between the syntax's parentheses. The 
  * function's parameters act as placeholders, or slots waiting to be filled with the argument values. 
  * 
- * 1. 
+ * 1. The two phases to using functions: First we must declare. Next we can execute (e.g. invoke, or call) a function by placing arguments in the parentheses or placeholders.
  * 
- * 2. 
+ * 2. What's the difference between a function's parameters and arguemnts PASSED to a function?
  * 
- * 3: 
+ * 3. What's the syntax for a NAMED function?
+ * 
+ * 4. How do we assign a function to a variable?
+ * 
+ * 5. Functions can OPTIONALLY take inputs, and OPTIONALLY return a single value. How do we specify inputs, and how do we specify outputs?
+ * 
+ * 6. Scope: Functions can see and modify variables in parent or global scopes. The inverse is NOT true.
+ * 
+ * 7. Closures: Functions for closures around the data they house. If an object returned from the Function and is held in memory somewhere (referenced), that closure stays ALIVE, and data can continue to exist in these closures! 
  */
