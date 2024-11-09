93ecd313508eda7f69cbbeb88dfbe656b6a15406
mpcooper23 <mpcooper23@gmail.com>
2024-11-08 23:14:49 -0600

    w

--- variables.js
+++ variables.js
@@ -1,11 +1,11 @@
 /**
- * 0: Variables are named containers for storing data. (... add a few more sentences to describe variables)
+ * 0: Variables are named containers for storing data that are scoped. 
  * 
  * 1. Declaration and Assignment: In order to create a variable we must first `declare` it. Depending on the declarative keyword you use, you may initialize
  * the variable with a value or not. 
  * 
  * 2. Var, let, const: There are three declarative keywords that can be used to declare variables--var, let, const. (.. describe each keyword in detai)
  * 
- * 3: Hoisting: (... describe hoisting in detail)
+ * 3: Hoisting: During the Javascript reader's compilation phase, the program scans the data and pulls up certain variables, prioritizing named functions and varaibles declared with "var". Ultimately, the prioritized data that is pulled to the top of the code script, or "hoisted", is read first during the reading phase.  
  */
 
