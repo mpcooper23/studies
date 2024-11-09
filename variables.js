c48d81ca1db66760de6e097eebf2c48bcf71e8c3
mpcooper23 <mpcooper23@gmail.com>
2024-11-08 23:22:25 -0600

    q

--- variables.js
+++ variables.js
@@ -1,11 +1,20 @@
 /**
- * 0: Variables are named containers for storing data that are scoped. 
+ * 0: Variables are named containers for storing data and are scoped. One might think of a variable 
+ * like a tentacle that points to a piece of data. This means that a variable that is globally scoped 
+ * can be invoked or 'reached' at any subsequent point in the program.
  * 
- * 1. Declaration and Assignment: In order to create a variable we must first `declare` it. Depending on the declarative keyword you use, you may initialize
- * the variable with a value or not. 
+ * 1. Declaration and Assignment: In order to create a variable we must first `declare` it. 
+ * Depending on the declarative keyword you use, you may initialize the variable with a value or not. 
+ * For instance, "var" 
  * 
- * 2. Var, let, const: There are three declarative keywords that can be used to declare variables--var, let, const. (.. describe each keyword in detai)
+ * 2. Var, let, const: There are three declarative keywords that can be used to declare variables--var, let, const. 
  * 
- * 3: Hoisting: During the Javascript reader's compilation phase, the program scans the data and pulls up certain variables, prioritizing named functions and varaibles declared with "var". Ultimately, the prioritized data that is pulled to the top of the code script, or "hoisted", is read first during the reading phase.  
+ * // => "var":
+ * // => "let":
+ * // => "const":
+ * 
+ * 3: Hoisting: During the Javascript reader's compilation phase, the program scans the data and pulls up certain variables, 
+ * prioritizing named functions and varaibles declared with "var". Ultimately, the prioritized data that is pulled to the 
+ * top of the code script, or "hoisted", is read first during the reading phase.  
  */
 
