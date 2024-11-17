0e79661214fae6b342afd2d4d8146dc1832a349e
mpcooper23 <mpcooper23@gmail.com>
2024-11-17 16:12:14 -0600

    q

--- datatypes.js
+++ datatypes.js
@@ -1,30 +1,40 @@
 /**
  * 
- *  * Data Types
+ *  ***********************=> Data Types <=***************************
  * 
- * 0: 
+ * 0: Variable declarations bind data. There are simple and complex data types.
+ * Simple are static and complex can mutate. 
  * 
- * 1. Number:
+ * 1. Number: intergers, which include those that use decimals 
+ *
+ * 2. String: characters bracketed by quotations. You may use either single or 
+ * double quotes, but be consistent with your usage. 
  * 
- * 2. String:
+ * 3. Boolean: true or false values. Think of this type of data as a sort of 
+ * light switch, whereby you can instruct the computer to take different paths
+ * depending on specific boolean conditions. 
  * 
- * 3. Boolean:
+ * 4. Array: zero-indexed, bucket-like container. This complex data type stores other 
+ * simple data types, as well as functions. We use bracket notation to access each element. 
+ * Elements start at the 0 index. We use for loops to iterate through array elements.
  * 
- * 4. Array:
+ * 5. Object: unlike arrays, which are zero-indexed based, objects store key:value pairs,
+ * which can be accessed using dot notation or bracket notation. We use for-in loops to access
+ * each key in the object. 
  * 
- * 5. Object:
- * 
- * 6. Function:
+ * 6. Function: block-scoped and first-class objects in that they can be plugged into other 
+ * functions (i.e. higher-order functions) and set as a value to a variable. 
  * 
  * 7. undefined:
  * 
  * 8. null:
  * 
- * 9. NAN:
+ * 9. NAN: "Not a Number"
  * 
  * 10. Infinity and -Infinity: 
  * 
  * 11. What is the difference between primitive/simple and complex data types?
  * 
- * 12. Primitive values are passed to a function BY COPY, complex values are BY REFERENCE. What does that mean, and how are they different?
+ * 12. Primitive values are passed to a function BY COPY, complex values are BY REFERENCE. 
+ * What does that mean, and how are they different?
  */
