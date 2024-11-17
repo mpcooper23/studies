a00bd83d2646d2c2e96f5ee551a92769525679ae
mpcooper23 <mpcooper23@gmail.com>
2024-11-17 17:02:44 -0600

    1

--- loops.js
+++ loops.js
@@ -1,9 +1,29 @@
 /**
  *********************************=> Loops <=************************************
  * 
- * 0: 
+ * 0: aptly-named, loops iteratate through complex data such as arrays and objects. We use specific types of 
+ * loops depending on the data type, the times we want to loop through the item, and other factors such as 
+ * knowing the length property or the amount of keys in an object. 
  * 
- * 1. Explain while, for, and for-in loops.
+ * When we iterate through an array or object, it's helpful to think in terms of each loop iteration 
+ * "isolating" a singular element at a time to perform whatever action is specified in the loop's code block.
+ * 
+ * The loop's syntax includes a keyword, followed by parenthesis, which is then followed by a code block.
+ * 
+ * 1. => while loops: best for instances where we want the loop to iterate and run its code block at least once,
+ * these loops are used when we don't know exactly how many times we want to iterate. It's important to 
+ * specify a incremental/decremental condition, or an inifinite loop will occur, crashing the program.
+ * 
+ * => for loops: conversely to the while loop, a for loop is used when we know how many times we want to iterate
+ * through a piece of data. We can set a stopping point in our conditions to indicate the number of iterations the 
+ * loop performs. A third condition is set to specify the incremental/decremental direction. 
+ * 
+ * => for-in loops: used to iterate through objects, the for-in loop's syntax triggers iteration through each key 
+ * in a given object. As each key is isolated, we can retrieve its value, or manipulate individual elements within
+ * the object
+ * 
+ * 
+ * 
  * 
  * 2. Be able to loop any number of times, forward counting up to some limit, backward counting down to 0.
  * 
