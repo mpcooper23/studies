40adf8bc63dc8d455089089815b06551ffbf47e5
mpcooper23 <mpcooper23@gmail.com>
2024-11-23 09:32:47 -0600

    w

--- control-flow.js
+++ control-flow.js
@@ -4,17 +4,38 @@
  * 0: Control flow refers to the pathways developers create with conditional 
  * statements. 
  * 
- * 1. If: the first condition in a conditional chain uses the if keyword followed 
+ * 1. If: individual if statements are entities within themselves, meaning they are 
+ * treated independently. It will always execute if it's condition is true. 
+ */
+
+ if (1 < 2) {
+console.log('if statement 1')
+ }
+
+ if (1 < 3){
+console.log('if statement 2')
+ }
+
+ /* Otherwise, the first condition in a conditional chain uses the if keyword followed 
  * by paranthesis between which we set the boolean according to our logic. 
- * 
- * 2. Else-if: this condition always follows the first "if" statement. 
+ */ 
+
+if (1<2) {
+    console.log('if "if" is true, then the conditional chain will not pass this initial statement')
+}else if (1<3){
+    console.log('even though this condition is true, it will never run because "if" has already ended chain')
+}else (1<4){
+    console.log('another true condition that does not run because the first condition, "if", stops chain')
+}
+
+ /* 2. Else-if: this condition always follows the first "if" statement. 
  * 
  * 3. Else: this condition does not require parentheses (i.e. a set condition)
  * 
  * 4. Switch: this statement
  * 
  * 
- * 
+ * 5. Ternary Operator: shorthand for if-else statement; use this to acheive DRY, cleaner code. 
  * 
  * 
  */
