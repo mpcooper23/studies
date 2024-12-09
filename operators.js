30bfc693ea799c99dc9a24699e8a48e4e9b4d39c
mpcooper23 <mpcooper23@gmail.com>
2024-12-09 13:05:59 -0600

    1

--- operators.js
+++ operators.js
@@ -1,9 +1,10 @@
 /**
  ************************=> Operators <=***************************
  * 
- * 0: We use these to perform different types of mathematical and logical computations.
+ * 0: We use these to perform different types of mathematical and logical computations. An "operand" is the data being 
+ * operated on. The "operand" combined with the "operator" makes the "operatrion".
  * 
- * 1. Assignment Operators: "assigns" values.
+ * 1. Assignment Operators: "assigns" values. These include the basic initializing "=" sign.
  * 
  * 2. Arithmetic Operators: perform "arithemtic" to return a single value. These include traditional mathematical operators
  * such as +, -, *, / (add, subtract, multiply, divide, respectively). 
@@ -15,7 +16,7 @@
  * 
  * ==> Decrement: -- (typically used as decremental indicator in looping: "i--")
  * 
- * ==> Exponentiation: ** ()
+ * ==> Exponentiation: ** (calculates the base to the exponent power)
  * 
  * 3. Comparison Operators: "compares" values. These include the traditional "greater than", "less than", "greater or equal to", 
  * and the "less or equal to" operators. 
@@ -37,8 +38,33 @@
  * ==> ! (Logical Not)
  * 
  * 
- * 5. Unary Operators (!, typeOf, -):
+ * 5. Unary Operators (!, typeOf, -): These require only one operand for operation
  * 
- * 6. Ternary Operators (a ? b: c):
+ * ==> ! (the bang operator): a unary logical operator, this negates a truthy boolean value
  * 
+ * ==> typeOf (the "type of" operator): returns the data type of a value
+ */
+
+typeof "hello" //string
+typeof true //boolean
+
+ /*
+ * ==> - (the unary negative): used to convert operand to negative number, if it isn't already negative. 
+ * 
+ * 6. Ternary Operators (a ? b: c): this is the only operator in Javascript that has three operands. 
+ * The ternary operator's syntax corresponds to a conditional "if", "if-else", "else" statement, first presenting 
+ * the conditional statement, and then including what to log if true, and what to log if false. 
+ * 
+ * Here is the ternary syntax, as it corresponds to the following: a condition is followed by a question mark (?), 
+ * then an expression to execute if the condition is truthy, and then a second expression for falsy values. These last two
+ * parts are separated by a colon (:), as such:
+ */ 
+
+function getFee(isMember){ //function that checks if club members have already paid annual fees.
+return isMember ? "$2.00" : "$10.00" //logs 2$ if user is a member; logs 10$ otherwise.
+}
+
+ /* 
+ The shortened syntax that the ternary operator offers aligns to the DRY concept: Do not repeat yourself, as is
+ typically the case with lengthy if-else statemets. 
  */
