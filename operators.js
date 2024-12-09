02222a5a5999997d497db8d7932fe6019af01c18
mpcooper23 <mpcooper23@gmail.com>
2024-12-09 10:33:32 -0600

    1

--- operators.js
+++ operators.js
@@ -1,18 +1,44 @@
 /**
  ************************=> Operators <=***************************
  * 
- * 0: 
+ * 0: We use these to perform different types of mathematical and logical computations.
  * 
- * 1. Assignment Operators:
+ * 1. Assignment Operators: "assigns" values.
  * 
- * 2. Arithmetic Operators:
+ * 2. Arithmetic Operators: perform "arithemtic" to return a single value. These include traditional mathematical operators
+ * such as +, -, *, / (add, subtract, multiply, divide, respectively). 
  * 
- * 3. Comparison Operators:
+ * ==> Remainder: % (typically used to check if number is odd or even: "i % 2 === 0" would test for an even value). The remainder
+ * operator is used to divide a number to see if there is a remainder, or if the number divides evenly. 
  * 
- * 4. Logical Operators: 
+ * ==> Increment: ++ (typically used as incremental indicator in looping: "i++")
+ * 
+ * ==> Decrement: -- (typically used as decremental indicator in looping: "i--")
+ * 
+ * ==> Exponentiation: ** ()
+ * 
+ * 3. Comparison Operators: "compares" values. These include the traditional "greater than", "less than", "greater or equal to", 
+ * and the "less or equal to" operators. 
+ * 
+ * ==> == (loosely equal to)
+ * 
+ * ==> === (strictly equal to)
+ * 
+ * ==> != (not equal)
+ * 
+ * ==> !== (not equal value && not equal data type)
+ * 
+ * 4. Logical Operators: used in boolean contexts to test for true/false. 
+ *
+ * ==> && (and)
+ * 
+ * ==> || (or)
+ * 
+ * ==> ! (Logical Not)
+ * 
  * 
  * 5. Unary Operators (!, typeOf, -):
  * 
- * Ternary Operators (a ? b: c):
+ * 6. Ternary Operators (a ? b: c):
  * 
  */
